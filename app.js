2a62cf9ff54550b3be50e03dc4ce38827fc2af94
Adaeze Dawari <78680916+Ebulor@users.noreply.github.com>
2021-12-10 09:42:57 +0400

    j

--- app.js
+++ app.js
@@ -11,18 +11,18 @@ fetch("data.json")
 
 //create element
 
-const mainContainer = document.getElementById("filter");
-
 //appending
 
 function createCard(data) {
-  for (var i = 0; i < data.length; i++) {
+  for (let i = 0; i < data.length; i++) {
+    const mainContainer = document.getElementById("filter");
     //create elements
     const profile = document.createElement("div");
     const content = document.createElement("div");
+    const tags = document.createElement("div");
     const logo = document.createElement("img");
     const textContent = document.createElement("div");
-    const texTop = document.createElement("div");
+    const textTop = document.createElement("div");
     const company = document.createElement("p");
     const textCenter = document.createElement("div");
     const position = document.createElement("h3");
@@ -30,6 +30,8 @@ function createCard(data) {
     const postedAt = document.createElement("p");
     const contract = document.createElement("p");
     const location = document.createElement("p");
+    const role = document.createElement("button");
+    const level = document.createElement("button");
 
     //set
     logo.src = data[i].logo;
@@ -38,26 +40,46 @@ function createCard(data) {
     postedAt.innerHTML = data[i].postedAt;
     contract.innerHTML = data[i].contract;
     location.innerHTML = data[i].location;
+    role.innerHTML = data[i].role;
+    level.innerHTML = data[i].level;
 
     //append
     mainContainer.appendChild(profile);
     profile.appendChild(content);
+    profile.appendChild(tags);
     content.appendChild(logo);
     content.appendChild(textContent);
-    textContent.appendChild(texTop);
-    texTop.appendChild(company);
+    textContent.appendChild(textTop);
+    textTop.appendChild(company);
     textContent.appendChild(textCenter);
     textCenter.appendChild(position);
     textContent.appendChild(textBottom);
     textBottom.appendChild(postedAt);
     textBottom.appendChild(contract);
     textBottom.appendChild(location);
+    tags.appendChild(role);
+    tags.appendChild(level);
 
     //new and featured
     if (data[i].new === "true") {
       const newBtn = document.createElement("button");
-      newBtn.innerHtml = "new!";
-      texTop.appendChild(newBtn);
+      newBtn.innerText = "new!";
+      textTop.appendChild(newBtn);
+      console.log(newBtn);
+    }
+
+    //creating languages
+    for (let j = 0; j < data[i].languages.length; j++) {
+      const langTag = document.createElement("button");
+      langTag.innerText = data[i].languages[j];
+      tags.appendChild(langTag);
+    }
+
+    //creating tools
+    for (let k = 0; k < data[i].tools.length; k++) {
+      const toolsTag = document.createElement("button");
+      toolsTag.innerText = data[i].tools[k];
+      tags.appendChild(toolsTag);
     }
 
     //adding classes
